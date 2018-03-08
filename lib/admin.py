d018d98b2cabfd5dc907531e63d9200f4bfea7f7
adel_mandour <adel_mohy58@hotmail.com>
2018-03-08 17:24:10 +0200

    Base

--- lib/admin.py
+++ lib/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+admin.site.register(Book)
+
+from .models import Auther
+admin.site.register(Auther)
+
+from .models import User
+admin.site.register(User)
